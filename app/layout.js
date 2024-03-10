6eadba4a90548ebacc3db000a2cb699d2050733e
Damjan15 <stamenovdamjan5@gmail.com>
2024-03-10 12:30:59 +0100

    feat: navbar component created

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "PropertyPulse",
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
