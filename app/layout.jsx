841ae1ae19fbab35fd0770265a44ca5ac3818d2d
Damjan15 <stamenovdamjan5@gmail.com>
2024-04-23 11:29:46 +0200

    feat: AuthProvider added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,23 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 import "@/assets/styles/globals.css";
 
 export const metadata = {
   title: "PropertyPulse",
   description: "Find The Perfect Rental Property",
-}
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
