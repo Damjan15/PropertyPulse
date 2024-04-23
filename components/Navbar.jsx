5784642c0f4fe24ed16f32fa99fb7d234f12d4c5
Damjan15 <stamenovdamjan5@gmail.com>
2024-04-23 11:48:01 +0200

    feat: sign out functionality added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -197,15 +197,18 @@ const Navbar = () => {
                     >
                       Saved Properties
                     </Link>
-                    <Link
-                      href="/"
+                    <button
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut();
+                      }}
                       className="block px-4 py-2 text-sm text-gray-700"
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-2"
                     >
                       Sign Out
-                    </Link>
+                    </button>
                   </div>
                 )}
               </div>
