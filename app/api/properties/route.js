2e81dcb9bdc29a8c6c99cb9ff55431a16360856a
Damjan15 <stamenovdamjan5@gmail.com>
2024-04-23 13:11:16 +0200

    feat: added utility function for getting the user session, saving posting to database

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
 
 // Get /api/properties
 export const GET = async (request) => {
@@ -17,6 +18,16 @@ export const GET = async (request) => {
 // Post /api/properties
 export const POST = async (request) => {
   try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+        return new Response('User ID is required', { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
     const formData = await request.formData();
 
     // Acces all values for amenities and images
@@ -51,13 +62,13 @@ export const POST = async (request) => {
         phone: formData.get("seller_info.phone"),
       },
       images,
+      owner: userId,
     };
 
-    console.log(propertyData);
+    const newProperty = new Property(propertyData);
+    await newProperty.save();
 
-    return new Response(JSON.stringify({ message: "Success" }), {
-      status: 201,
-    });
+    return Response.redirect(`${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`);
   } catch (error) {
     return new Response("Failed to add property", { status: 500 });
   }
