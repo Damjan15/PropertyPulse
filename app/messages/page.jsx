5a8eb2cd076f1f7d20ec205b1080852dba42961a
Damjan15 <stamenovdamjan5@gmail.com>
2024-04-29 12:40:54 +0200

    feat: updated Messages component, created new Message component, rendering messages, middleware updated with routes

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -1,5 +1,5 @@
-import Message from "@/components/Message"
+import Messages from "@/components/Messages"
 
 export default function MessagesPage() {
-    return <Message />
+    return <Messages />
 }
--- components/Message.jsx
+++ components/Message.jsx
@@ -1,34 +1,45 @@
-"use client";
-
-import { useState, useEffect } from "react";
-import Spinner from "./Spinner";
-
-
-const Message = () => {
-    const [messages, setMessages] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const getMessages = async () => {
-            try {
-                const res = await fetch(`/api/messages`);
-
-                if (res.status === 200) {
-                    const data = await res.json();
-                    setMessages(data);
-                }
-            } catch (error) {
-                console.log("Error fetching messages: ", error);
-            } finally {
-                setLoading(false);
-            }
-        }
-
-        getMessages();
-    }, []);
+const Message = ({ message }) => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="relative bg-white p-4 rounded-md shadow-md border border-gray-200">
+      <h2 className="text-xl mb-4">
+        {" "}
+        <span className="font-bold">Property Inquiry:</span>{" "}
+        {message.property.name}
+      </h2>
+      <p className="text-gray-700 text-lg">{message.body}</p>
 
-export default Message
+      <ul className="mt-4">
+        <li>
+          <strong>Name:</strong> {message.name}
+        </li>
+
+        <li>
+          <strong>Reply Email:</strong>
+          <a href="mailto:recipient@example.com" className="text-blue-500">
+            {" "}
+            {message.email}
+          </a>
+        </li>
+        <li>
+          <strong>Reply Phone:</strong>
+          <a href={`tel:${message.phone}`} className="text-blue-500">
+            {" "}
+            {message.phone}
+          </a>
+        </li>
+        <li>
+          <strong>Received:</strong>{" "}
+          {new Date(message.createdAt).toLocaleString()}
+        </li>
+      </ul>
+      <button className="mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md">
+        Mark As Read
+      </button>
+      <button className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md">
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Message;
